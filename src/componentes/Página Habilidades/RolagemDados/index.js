dda5ac8f02d1ade2c1cd1dc5a6f991c8d58e33aa
Gamirca <g.cangussu@hotmail.com>
2022-08-03 18:28:58 -0300

    Update index.js

--- src/componentes/Página Habilidades/RolagemDados/index.js
+++ src/componentes/Página Habilidades/RolagemDados/index.js
@@ -1,10 +1,18 @@
 import './rolagem.css'
+import { useState } from 'react'
 
 const RolagemDados = (props) => {
 
+
+      const [dado, setDado] = useState('')
+      let rolagem1 = [Math.floor(Math.random() * 6), Math.floor(Math.random() * 6), Math.floor(Math.random() * 6), Math.floor(Math.random() * 6)]
+      let rolagemFinal = rolagem1.reduce((pv, cv) => pv + cv)
+
+
       return (
+
             <section>
-                  <div className='dado'>{Math.floor(Math.random() * 6) + 1}</div>
+                  <div className='dado'>{ }</div>
             </section>
       )
 }
